04ab851b9fcbebb835511e649eefe4709b2583bd
carlosjs99 <carlosjs999@gmail.com>
2022-05-13 13:53:16 +0100

    Kata terminada en TypeScript

--- __test__/romanNumbers.test.ts
+++ __test__/romanNumbers.test.ts
@@ -15,4 +15,39 @@ describe('Roman numbers', () => {
     const romanNumber = convert(5);
     expect(romanNumber).toBe('V');
   });
+
+  it('should convert 100 to C', () => {
+    const romanNumber = convert(100);
+    expect(romanNumber).toBe('C');
+  });
+
+  it('should convert 4 to IV', () => {
+    const romanNumber = convert(4);
+    expect(romanNumber).toBe('IV');
+  });
+
+  it('should convert 6 to VI', () => {
+    const romanNumber = convert(6);
+    expect(romanNumber).toBe('VI');
+  });
+
+  it('should convert  12 to XII', () => {
+    const romanNumber = convert(12);
+    expect(romanNumber).toBe('XII');
+  });
+
+  it('should convert  543 to DXLIII', () => {
+    const romanNumber = convert(543);
+    expect(romanNumber).toBe('DXLIII');
+  });
+
+  it('should convert  410 to CDX', () => {
+    const romanNumber = convert(410);
+    expect(romanNumber).toBe('CDX');
+  });
+
+  it('should convert  484 to CDLXXXIV', () => {
+    const romanNumber = convert(484);
+    expect(romanNumber).toBe('CDLXXXIV');
+  });
 });
--- src/romanNumbers.ts
+++ src/romanNumbers.ts
@@ -1,16 +1,35 @@
 export const convert = (cardinal: number): string => {
-  if (cardinal === 2){
-    return 'II';
-  }
-  return ROMAN_VALUES[cardinal];
-};
 
+  let result: string = '';
+  const ROMAN_VALUES_REVERSE = Object.keys(ROMAN_VALUES).reverse();
+
+  ROMAN_VALUES_REVERSE.forEach(value => {
+    const key = parseInt(value);
+    while (cardinal >= key){
+      cardinal -= key;
+      result += ROMAN_VALUES[key];
+    }
+  });
+  return result;
+};
 
 const ROMAN_VALUES = {
   1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
   5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
   10: 'X',
+  40: 'XL',
+  50: 'L',
+  90: 'XC',
   100: 'C',
+  400: 'CD',
   500: 'D',
+  900: 'CM',
   1000: 'M'
 };
